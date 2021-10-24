3a62ebbbc21c2c0978c0bcdaad2e83850499c2cf
VarZagus <69350747+VarZagus@users.noreply.github.com>
2021-10-24 18:05:16 +0300

    Initial commit

---
+++ src/main/java/ru/team/web/dot/dotheroes/DotHeroesApplication.java
@@ -0,0 +1,13 @@
+package ru.team.web.dot.dotheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotHeroesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotHeroesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/team/web/dot/dotheroes/DotHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.team.web.dot.dotheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotHeroesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
